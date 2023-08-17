0e4c63979f8944bfd1826edccca331430e8974e2
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-08-17 16:07:22 -0300

    nav changes

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,8 @@
 export const NavLinks = [
-  { href: '/', key: 'Home', text: 'Home' },
   { href: '/explore', key: 'Explore', text: 'Explore' },
+  { href: '/projetos', key: 'Projetos', text: 'Projetos' },
+  { href: '/', key: 'Home', text: 'Home' },
   { href: '/architects', key: 'Architects', text: 'Arquitetos' },
   { href: '/profile?id=1', key: 'Profile', text: 'Perfil' },
+  { href: '/login', key: 'Login', text: 'Login' }
 ]
