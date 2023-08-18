4fa18f247bd628f75ffc26c4b9e3704ec1358647
gseven-gdv <zerogravitystd@gmail.com>
2023-08-18 15:32:22 -0300

    database integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
 import Image from 'next/image'
 import scssVariables from './styles/variables.module.scss'
+import prisma from '@/lib/prisma'
 
 console.log(scssVariables.primaryColor)
 
-export default function Home() {
+const getStaticProps = async () => {
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  })
+
+  return feed
+}
+
+export default async function Home() {
+  const data = await getStaticProps()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello, World!</h1>
+      <h1>{data.map((e) => e.title)}</h1>
     </main>
   )
 }
