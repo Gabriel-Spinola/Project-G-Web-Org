f3f4b25135a300026a058fe5cb6269fbe8666d47
gseven-gdv <zerogravitystd@gmail.com>
2023-08-21 16:13:11 -0300

    sa

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,5 @@
 // Without a defined Matcher, applies next auth to the entire project
 export { default } from 'next-auth/middleware'
 
-console.log('Middleware?')
-
 // Applies next-auth only to specific pages
 export const config = { matcher: ['/client/explore'] }
