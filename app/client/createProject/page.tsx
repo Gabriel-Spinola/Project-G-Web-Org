5c47d2ddefc739afc7839b804e85dece733aae1d
gseven-gdv <zerogravitystd@gmail.com>
2023-08-24 16:28:57 -0300

    update db and new directories

--- app/client/createProject/page.tsx
+++ app/client/createProject/page.tsx
@@ -1,27 +1,8 @@
-'use client'
-
 import CreateProjectForm from '@/components/projects/CreateProjectForm'
+import { Prisma } from '@prisma/client'
 import { FormEvent } from 'react'
 
 export default function CreateProject() {
-  const submitForm = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-
-    const formData = new FormData(event.currentTarget)
-    const response = await fetch('/api/handlers/', {
-      method: 'POST',
-      body: formData,
-    })
-
-    if (response.ok) {
-      const data = await response.json()
-
-      console.log(JSON.stringify(data))
-    } else {
-      console.log('Error submitting form data')
-    }
-  }
-
   return (
     <>
       <br />
@@ -29,19 +10,7 @@ export default function CreateProject() {
       <br />
       <br />
       <br />
-      <form onSubmit={submitForm} method="POST">
-        <label htmlFor="title">Title</label>
-        <input type="text" id="title" name="title" /> <br /> <br />
-        <label htmlFor="project-description">project-description</label>
-        <textarea
-          id="project-description"
-          name="project-description"
-        ></textarea>{' '}
-        <br /> <br />
-        <label htmlFor="project-img">project-description</label>
-        <input type="file" id="project-img" name="project-img" />
-        <button type="submit">Submit</button>
-      </form>
+      <CreateProjectForm />
     </>
   )
 }
--- components/projects/CreateProjectForm.tsx
+++ components/projects/CreateProjectForm.tsx
@@ -3,21 +3,42 @@
 import { useRouter } from 'next/navigation'
 import { FormEvent, useState } from 'react'
 
-function createProjectVoidFunction(): null {
-  return null
-}
-
 export default function CreateProjectForm() {
-  const router = useRouter()
-  const [submitting, setSubmitting] = useState<boolean>(false)
+  const submitForm = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
 
-  function handleFormSubmit(e: FormEvent) {
-    e.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const response = await fetch('/api/handlers/CreateProjectFormHandler/', {
+      method: 'POST',
+      body: formData,
+    })
 
-    setSubmitting(true)
-    createProjectVoidFunction()
-    router.push('/')
+    if (response.ok) {
+      const data = await response.json()
+
+      // TODO: Client Response
+      console.log(JSON.stringify(data))
+    } else {
+      // TODO: Client Response
+      console.log('Error submitting form data')
+    }
   }
 
-  return <></>
+  return (
+    <>
+      <form onSubmit={submitForm} method="POST">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" /> <br /> <br />
+          <label htmlFor="project-description">project-description</label>
+          <textarea
+            id="project-description"
+            name="project-description"
+          ></textarea>{' '}
+          <br /> <br />
+          <label htmlFor="project-img">project-description</label>
+          <input type="file" id="project-img" name="project-img" />
+          <button type="submit">Submit</button>
+        </form>
+    </>  
+  )
 }
