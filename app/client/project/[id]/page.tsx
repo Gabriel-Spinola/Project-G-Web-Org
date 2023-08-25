87bdaba41f3e4eacbb0bf6d0cfde658346203ed5
gseven-gdv <zerogravitystd@gmail.com>
2023-08-25 16:50:18 -0300

    added possible solutions to services-handler review

--- app/client/project/[id]/page.tsx
+++ app/client/project/[id]/page.tsx
@@ -1,5 +1,6 @@
 // REVIEW this page should be a server component.
 // It should get the data from database check it, and then display the components accordingly
+
 'use client'
 
 import DisplayProject from '@/components/projects/DisplayProject'
@@ -8,21 +9,21 @@ import { useRouter } from 'next/navigation'
 import React from 'react'
 
 type Props = {
-  params: {
-    id: string
-  }
+	params: {
+		id: string
+	}
 }
 
 // NOTE: Rule::Sensitive Info receive from API, other data from client
 const Project = ({ params }: Props) => {
 	const router = useRouter()
 
-  async function deleteProjectButtonHandler(event: React.MouseEvent<HTMLButtonElement>) {
-    event.preventDefault()
+	async function deleteProjectButtonHandler(event: React.MouseEvent<HTMLButtonElement>) {
+		event.preventDefault()
 
-    const response = await fetch(`/api/services/delete/?id=${params.id}&modelCode=${ModelsApiCode.Project}`, {
-      method: 'DELETE',
-    });
+		const response = await fetch(`/api/services/delete/?id=${params.id}&modelCode=${ModelsApiCode.Project}`, {
+			method: 'DELETE',
+		});
 
 		if (response.ok) {
 			router.push('/')
@@ -30,16 +31,17 @@ const Project = ({ params }: Props) => {
 		else {
 			console.log('Deletion Failed')
 		}
-  }
+	}
 
-  return (
-    <main>
-      <DisplayProject id={params.id} />
+	return (
+		<main>
+			<DisplayProject id={params.id} />
 
-      <button>edit</button>
-      <button onClick={deleteProjectButtonHandler}>delete</button>
-    </main>
-  )
+			<button>edit</button>
+			<br />
+			<button onClick={deleteProjectButtonHandler}>delete</button>
+		</main>
+	)
 }
 
 export default Project
