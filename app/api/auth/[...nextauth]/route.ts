7199dd9ca7dc11e72a0a745dc96a3462a05d8b31
gseven-gdv <zerogravitystd@gmail.com>
2023-08-28 15:35:51 -0300

    useful lnks

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,7 @@
+// LINK: https://codevoweb.com/setup-and-use-nextauth-in-nextjs-13-app-directory/#google_vignette
+// LINK: https://github.com/mikemajara/nextjs-prisma-next-auth-credentials/blob/main/pages/api/auth/%5B...nextauth%5D.ts
+
+
 import NextAuth from 'next-auth'
 import { AuthOptions } from './options'
 
