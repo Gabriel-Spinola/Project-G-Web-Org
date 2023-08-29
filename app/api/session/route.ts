b247c927a86b5daee95c20cad9353d42414d5b5e
gseven-gdv <zerogravitystd@gmail.com>
2023-08-29 15:31:32 -0300

    signin

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -7,6 +7,13 @@ type SessionResponse = NextResponse<Record<string, any>>
 export async function GET(req: Request): Promise<SessionResponse> {
 	const session = await getServerSession(AuthOptions)
 
+	if (!session) {
+		return new NextResponse(
+			JSON.stringify({ status: "fail", message: "You are not logged in" }),
+			{ status: 401 }
+		);
+	}
+
 	return NextResponse.json({
 		//               session != null ? true : false
 		isAuthenticated: !!session,
