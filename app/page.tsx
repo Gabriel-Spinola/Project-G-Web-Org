d2924ff2aad592481c40c033201cfdf1ca324012
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-08-29 21:04:08 -0300

    Modal Update

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Postbyuser from '@/components/Posts/Post'
 import Poster from '@/components/Poster/Poster'
 import scssVariables from './styles/variables.module.scss'
 import { prisma } from '@/lib/database/prisma'
+import Component from '@/components/Poster/test'
 
 export default async function Home() {
   const feedData = await prisma.post.findMany({
@@ -22,7 +23,7 @@ export default async function Home() {
       <div className='feed flex flex-col items-center'>
         <Poster></Poster>
         <Postbyuser></Postbyuser>
-        {feedData.map((e) => e.author?.name)}
+        {/* {feedData.map((e) => e.author?.name)} */}
       </div>
     </main>
   )
--- components/Poster/Modal.tsx
+++ components/Poster/Modal.tsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 
-const Modal = ({isVisible}: { isVisible: boolean}) => {
-  if ( !isVisible ) return null;
+const Modal = ({ isVisible, onClose }: { isVisible: boolean; onClose: () => void }) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleOutSideClick = (event) => {
+      if (!event.target.classList.contains('notClose')) {
+        onClose(); // Chamando a função onClose para fechar o modal
+      }
+    };
+
+    window.addEventListener('mousedown', handleOutSideClick);
+
+    return () => {
+      window.removeEventListener('mousedown', handleOutSideClick);
+    };
+  }, [onClose]); // Adicionei o onClose como dependência para o useEffect
+
+  if (!isVisible) return null;
+
   return (
-    <div className=' fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center'>
-      <div className='w-[809px] flex flex-col'>
-        <button className='text-pure-white text-xl place-self-end'>X</button>
-        <div className='drop-shadow-sm bg-medium-gray text-darker-white text-xl p-8 rounded-[8px]'> 
-          Faça uma publicação
+    <div className=' fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center z-20'>
+      <div className='w-[809px] h-[192px] flex flex-col mt-[-20%] z-10'>
+        <button className='text-pure-white text-xl place-self-end' onClick={onClose}>X</button>
+        <div className='notClose z-1 drop-shadow-sm bg-medium-gray text-darker-white text-xl p-8 rounded-[8px]'>
+          <form className='notClose'>
+            <input type='text' placeholder='Faça uma publicação' className='notClose bg-medium-gray text-darker-white w-full pb-[192px] text-xl margin-none text-start outline-none'></input>
+            <div className='notClose max-h-[64px] flex flex-row justify-between'>
+              <div className='notClose'>
+                <button className='notClose flex flex-row mt-8 select-none text-sm hover:text-medium-primary'>
+                  <i className='notClose'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className='notClose mr-2'><path fill="currentColor" d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H8V4h12v12zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm12 6V9c0-.55-.45-1-1-1h-2v5h2c.55 0 1-.45 1-1zm-2-3h1v3h-1V9zm4 2h1v-1h-1V9h1V8h-2v5h1zm-8 0h1c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1H9v5h1v-2zm0-2h1v1h-1V9z" /></svg>
+                  </i>
+                  Envie um arquivo
+                  <input type='file' accept='.pdf' multiple className='notClose'></input>
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
