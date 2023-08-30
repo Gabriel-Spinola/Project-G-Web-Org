645c5d506ef8de7ef817d64fb631803841b76da4
Pingola <zerogravitystd@gmail.com>
2023-08-30 15:07:53 -0300

    fix merge errors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,20 +2,11 @@ import { NavLinks } from '@/constants'
 import Link from 'next/link'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
-import { AppProps } from 'next/app'
-import { getServerSession } from 'next-auth'
 import '@/app/styles/navbar.scss'
 
 async function Navbar() {
   return (
     <nav className='shadow-darker-white/40 shadow-2xl'>
-      {/* {session ? (
-        <h1>session?.user</h1>
-      ) : (
-        <h1 className="text-5xl">You&apos;re not logged in</h1>
-      )}
-       */}
-
       <ul>
         {NavLinks.map((link) => (
           // eslint-disable-next-line react/jsx-key
@@ -31,15 +22,13 @@ async function Navbar() {
               />}
             </Link>
           </li>
-        </div>
+        ))}
 
         <div className='right-wing flex flex-row'>
           {NavLinks.filter(link => link.key === 'Architects' || link.key === 'Profile').map(link => (
             <li key={link.key}>{link.text}</li>
           ))}
         </div>
-
-
       </ul>
     </nav>
   )
