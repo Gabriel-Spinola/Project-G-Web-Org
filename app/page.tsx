63437c84a5c8025fcb32dc0ddfb89883b65ab058
esther de oliveira <gabrielspinola77@gmail.com>
2023-08-31 15:55:43 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,6 @@ import PostSubmitFragment from '@/components/posts/poster/PostSubmitFragment'
 import { prisma } from '@/lib/database/prisma'
 
 export default async function Home() {
-  const feedData = await prisma.post.findMany({
-    where: { published: true },
-    include: {
-      author: {
-        select: { name: true },
-      },
-    },
-  })
-
   return (
     <main className="flex min-h-screen justify-around flex-row pt-24 bg-darker-white">
       <div className="feed flex flex-col items-center">
