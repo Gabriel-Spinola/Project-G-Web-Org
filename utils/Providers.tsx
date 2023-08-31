206f87cee955980751aa801b0af9081da0d2b1b7
esther de oliveira <gabrielspinola77@gmail.com>
2023-08-31 16:46:51 -0300

    -m

---
+++ utils/Providers.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import React from 'react'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
+import { SessionProvider } from 'next-auth/react'
+
+function Providers({ children }: React.PropsWithChildren) {
+  const [client] = React.useState(new QueryClient())
+
+  return (
+    <QueryClientProvider client={client}>
+      <ReactQueryStreamedHydration>
+        <SessionProvider>{children}</SessionProvider>
+      </ReactQueryStreamedHydration>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+export default Providers
