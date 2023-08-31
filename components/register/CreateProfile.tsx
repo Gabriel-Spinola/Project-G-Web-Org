f09aa448b653328314da7482d80275eba4d26eb5
gseven-gdv <zerogravitystd@gmail.com>
2023-08-31 10:14:13 -0300

    Create CreateProfile.tsx

---
+++ components/register/CreateProfile.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { Session } from 'next-auth'
+import { signIn } from 'next-auth/react'
+import React, { ChangeEvent, useState } from 'react'
+
+interface ICreateProfileProps {
+  session: Session | null
+  reloadSession: () => void
+}
+
+export default function CreateProfile({
+  session,
+  reloadSession,
+}: ICreateProfileProps) {
+  const [username, setUsername] = useState<string>(session?.user.name ?? '')
+
+  async function onSubmit() {
+    try {
+      /**
+       * mutation to createUsername into graphql api
+       */
+    } catch (error: unknown) {
+      console.error('onSubmit failed', error)
+    }
+  }
+
+  return (
+    <div className="flex text-center">
+      {session ? (
+        <>
+          <h1>Create User</h1>
+          <input
+            type="text"
+            value={username}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              setUsername(event.target.value)
+            }
+          />
+          <button onClick={onSubmit}>Salvar</button>
+        </>
+      ) : (
+        <>
+          <h1>Update Maybe</h1>
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+    </div>
+  )
+}
