005b6576f792d049e3c44831b647bd262a913729
gseven-gdv <zerogravitystd@gmail.com>
2023-08-31 09:55:30 -0300

    Inserting data into next-auth module

---
+++ lib/auth/next-auth.d.ts
@@ -0,0 +1,15 @@
+import 'next-auth'
+
+// ANCHOR Add into the existing interfaces in the next-auth
+declare module 'next-auth' {
+  interface User {
+    description?: string
+    siteUrl?: string
+    linkedinUrl?: string
+    contactPhone?: string
+  }
+
+  interface Session {
+    user: User
+  }
+}
