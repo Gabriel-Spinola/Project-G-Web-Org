e93a4eb8e6e24203df1be75ff4a41e12264d4f49
esther de oliveira <gabrielspinola77@gmail.com>
2023-08-31 14:46:32 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,8 @@
 
 import Postbyuser from '@/components/Posts/Post'
 import Poster from '@/components/Poster/Poster'
-import { prisma } from '@/lib/database/prisma'
 
 export default async function Home() {
-  const feedData = await prisma.post.findMany({
-    where: { published: true },
-    include: {
-      author: {
-        select: { name: true },
-      },
-    },
-  })
-
   return (
     <main className="flex min-h-screen justify-around flex-row pt-24 bg-darker-white">
       <div className="feed flex flex-col items-center">
