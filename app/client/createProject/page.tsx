4ed122a4526cdc8916ede1b9216dc1ffe2b9d4ef
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-08-31 13:30:40 -0300

    Create page.tsx

---
+++ app/client/createProject/page.tsx
@@ -0,0 +1,16 @@
+import CreateProjectForm from '@/components/projects/CreateProjectForm'
+import { Prisma } from '@prisma/client'
+import { FormEvent } from 'react'
+
+export default function CreateProject() {
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <CreateProjectForm />
+    </>
+  )
+}
