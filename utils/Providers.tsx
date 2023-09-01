8534731073dc13085d881b502ba8f09c1c45619b
gseven-gdv <zerogravitystd@gmail.com>
2023-09-01 13:54:53 -0300

    Settting up providers

--- utils/Providers.tsx
+++ utils/Providers.tsx
@@ -5,17 +5,23 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental'
 import { SessionProvider } from 'next-auth/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = React.useState(new QueryClient())
 
   return (
-    <QueryClientProvider client={client}>
-      <ReactQueryStreamedHydration>
-        <SessionProvider>{children}</SessionProvider>
-      </ReactQueryStreamedHydration>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <SessionProvider>
+      <ChakraProvider>
+        <QueryClientProvider client={client}>
+          <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+
+          <ReactQueryDevtools
+            initialIsOpen={process.env.NODE_ENV === 'development'}
+          />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </SessionProvider>
   )
 }
 
