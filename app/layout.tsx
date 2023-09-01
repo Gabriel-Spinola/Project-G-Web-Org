5363e17abecb8271ffa1762852494efa3f8985f0
gseven-gdv <zerogravitystd@gmail.com>
2023-09-01 14:13:35 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import AuthProvider from './context/AuthProvider'
 import Providers from '@/utils/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
