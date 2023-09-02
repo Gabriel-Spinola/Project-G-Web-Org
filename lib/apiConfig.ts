3790ca5822fa59c697ab74d437dde692e7399f66
gseven-gdv <zerogravitystd@gmail.com>
2023-09-02 19:49:01 -0300

    update basic user info

---
+++ lib/apiConfig.ts
@@ -0,0 +1,10 @@
+export const apiEndpoints = {
+  services: {
+    findUnique: '/api/services/find-unique/',
+    findMany: '/api/services/',
+  },
+  handlers: {
+    createProject: '/api/handlers/CreateProjectFormHandler/',
+    updateUser: '/api/handlers/update-user/',
+  },
+}
