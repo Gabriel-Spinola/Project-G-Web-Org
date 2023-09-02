7ea4ead52d6a58e6cd202255f334e232d4c9e60d
gseven-gdv <zerogravitystd@gmail.com>
2023-09-02 18:46:44 -0300

    Editable inputs

--- components/DisplayUserInfo.tsx
+++ components/DisplayUserInfo.tsx
@@ -1,11 +1,69 @@
+'use client'
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Editable,
+  EditablePreview,
+  EditableInput,
+  Divider,
+  FormLabel,
+  EditableTextarea,
+} from '@chakra-ui/react'
 import { User } from 'next-auth'
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 
 interface Params {
   user: User
 }
 
+const defaultEditFormValues = {
+  title: 'Insira seu titulo',
+  description: 'Insira uma descrição',
+}
+
 export default function DisplayUserInfo({ user }: Params): React.JSX.Element {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  async function handleFormSubmission(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    // Helper function to get a field's value or default to an empty string
+    const getFieldValueOrDefault = (
+      fieldName: string,
+      defaultValue: string,
+    ) => {
+      const fieldValue = formData.get(fieldName) as string | null
+
+      return fieldValue === defaultValue ? '' : fieldValue
+    }
+
+    // Define default values
+    const defaultTitle = defaultEditFormValues.title
+    const defaultDescription = defaultEditFormValues.description
+
+    // Update form data for 'title' field
+    formData.set('title', getFieldValueOrDefault('title', defaultTitle) ?? '')
+
+    // Update form data for 'description' field
+    formData.set(
+      'description',
+      getFieldValueOrDefault('description', defaultDescription) ?? '',
+    )
+
+    console.log(formData.get('title'))
+    console.log(formData.get('description'))
+  }
+
   return (
     <section id="user-info">
       <h1>{user?.name}</h1>
@@ -20,6 +78,63 @@ export default function DisplayUserInfo({ user }: Params): React.JSX.Element {
       <h1>Site: {user?.siteUrl}</h1>
       <h1>phone: {user?.contactPhone}</h1>
       <h1>email: {user?.email}</h1>
+
+      <Button onClick={onOpen}>Editar Usuário</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+
+          <form onSubmit={handleFormSubmission}>
+            <ModalBody>
+              <FormLabel>Name</FormLabel>
+              <Editable defaultValue={user?.name || ''}>
+                <EditablePreview />
+                <EditableInput type="text" name="name" id="name" />
+              </Editable>
+
+              <FormLabel>Title</FormLabel>
+              <Editable
+                defaultValue={user?.title || defaultEditFormValues.title}
+                isPreviewFocusable={true}
+              >
+                <EditablePreview />
+                <EditableInput
+                  display="insira um titolu"
+                  type="text"
+                  name="title"
+                  id="title"
+                />
+              </Editable>
+
+              <FormLabel>Description</FormLabel>
+              <Editable
+                defaultValue={
+                  user?.description || defaultEditFormValues.description
+                }
+              >
+                <EditablePreview />
+                <EditableTextarea name="description" id="description" />
+              </Editable>
+
+              <Divider />
+            </ModalBody>
+
+            <ModalFooter>
+              <Button variant="ghost" onClick={onClose}>
+                Cancelar
+              </Button>
+
+              <Button colorScheme="blue" mr={3} type="submit">
+                Salvar
+              </Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
     </section>
   )
 }
