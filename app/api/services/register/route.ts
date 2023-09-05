03a88461b677118f64ea927f2bbe23993d32dc2d
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-09-05 13:16:16 -0300

    Update route.ts

--- app/api/services/register/route.ts
+++ app/api/services/register/route.ts
@@ -1,3 +1,12 @@
+/**
+ * @author Gabriel Spinola Mendes da Silva | gabrielspinola77@gmail.com
+ * @author Lucas Vinicius Pereira Martis | lucasvinipessoal@gmail.com
+ *
+ * @project Project G
+ * @version main-release
+ * @license i.e. MIT
+ */
+
 import { prisma } from '@/lib/database/prisma'
 import { hash } from 'bcryptjs'
 import { NextResponse } from 'next/server'
