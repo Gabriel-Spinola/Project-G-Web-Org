c10fdf367d711165b7a1c63fe268809f6f4b6770
gseven-gdv <zerogravitystd@gmail.com>
2023-09-07 16:50:50 -0300

    removing werid past stuff

--- app/client/project/[id]/page.tsx
+++ app/client/project/[id]/page.tsx
@@ -7,80 +7,23 @@
  * @license i.e. MIT
  */
 
-// import { GetServerSideProps, GetStaticProps, NextPage } from 'next';
-// import { ModelsApiCode, ProjectModelProps } from '@/lib/database/table.types';
+import {} from 'next'
+import { ModelsApiCode, ProjectModelProps } from '@/lib/database/table.types'
 
-// type Props = {
-// 	data: ProjectModelProps | null;
-// };
+type Props = {
+  params: { id: string }
+}
 
-// export const getStaticProps: GetStaticProps<{ data: ProjectModelProps }> = async ({ params }) => {
-// 	const { id } = params || {};
+const Project = ({ params }: Props) => {
+  return (
+    <div className="project-container">
+      <h1>{params.id}</h1>
 
-// 	console.log('Server side props')
+      <button>edit</button>
+      <br />
+      <button>delete</button>
+    </div>
+  )
+}
 
-// 	if (id && typeof id === 'string') {
-// 		try {
-// 			const response = await fetch(
-// 				`/api/services/find-unique/?id=${id}&modelCode=${ModelsApiCode.Project}`,
-// 				{
-// 					method: 'POST',
-// 				}
-// 			);
-
-// 			console.log('trying')
-
-// 			if (response.ok) {
-// 				const { data } = await response.json();
-
-// 				return {
-// 					props: {
-// 						data,
-// 					},
-// 				};
-// 			} else {
-// 				throw new Error('Failed to fetch data');
-// 			}
-// 		} catch (error) {
-// 			console.error('Error fetching data:', error);
-// 		}
-// 	}
-
-// 	return {
-// 		props: {
-// 			data: null,
-// 		},
-// 	};
-// };
-
-// const Project: NextPage<Props> = ({ data }): => {
-// 	if (data) {
-// 		return (
-// 			<div className="project-container">
-// 				<br />
-// 				<br />
-// 				<br />
-// 				<br />
-
-// 				<h1>{data.title}</h1>
-// 				<span>{data.createdAt}</span>
-// 				<p>{data.description}</p>
-
-// 				{data.images.map((image: string) => (
-// 					<span key={image}>Image Names: {image}</span>
-// 				))}
-
-// 				<br />
-// 				<br />
-
-// 				<button>edit</button>
-// 				<br />
-// 				<button>delete</button>
-// 			</div>
-// 		);
-// 	}
-
-// 	return <div className="project-container"></div>;
-// };
-
-// export default Project;
+export default Project
