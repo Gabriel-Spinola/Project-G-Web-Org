e8579e8aa9d18c4a02eb22e128be77642e424ede
gseven-gdv <zerogravitystd@gmail.com>
2023-09-07 16:50:50 -0300

    removing werid past stuff

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,6 @@
 
 import PostItem from '@/components/Posts/Post'
 import PostSubmitFragment from '@/components/Posts/poster/PostSubmitFragment'
-import { API_ENDPOINTS, API_URL } from '@/lib/apiConfig'
-import { ModelsApiCode } from '@/lib/database/table.types'
 import { Post, User } from '@prisma/client'
 
 type PostResponse = { data: Array<Post> }
@@ -20,63 +18,8 @@ type PostAuthorData = Pick<User, 'name' | 'image' | 'graduations'>
 
 export type FullPost = Post & PostAuthorData
 
-async function getPostAuthorData(
-  authorId: string,
-): Promise<PostAuthorData | null> {
-  try {
-    const response = await fetch(
-      `${API_URL}${API_ENDPOINTS.services.findUnique}?id=${authorId}&modelCode=${ModelsApiCode.User}`,
-      { method: 'GET', headers: { 'Content-Type': 'application/json' } },
-    )
-
-    if (response.ok) {
-      const data: PostAuthorData = await response.json()
-
-      return data
-    }
-
-    throw new Error('Network response was not OK')
-  } catch (e: unknown) {
-    console.error('failed to load author data ', e)
-
-    return null
-  }
-}
-
 async function getPosts(): Promise<Array<FullPost> | null> {
-  try {
-    const response = await fetch(
-      `${API_URL}${API_ENDPOINTS.services.findMany}?modelCode=${ModelsApiCode.Post}`,
-      { method: 'GET', headers: { 'Content-Type': 'application/json' } },
-    )
-
-    if (response.ok) {
-      const { data }: PostResponse = await response.json()
-
-      const fullPosts = data.map(async (post: Post) => {
-        if (!post) return null
-
-        const postAuthor: PostAuthorData | null = await getPostAuthorData(
-          post.authorId as string,
-        )
-
-        return {
-          ...post,
-          name: postAuthor?.name as string | null,
-          image: postAuthor?.image as string | null,
-          graduations: postAuthor?.graduations as string[],
-        }
-      })
-
-      return fullPosts
-    }
-
-    throw new Error('Network response was not OK')
-  } catch (e: unknown) {
-    console.error('failed to load posts', e)
-
-    return null
-  }
+  return null
 }
 
 export default async function Home() {
