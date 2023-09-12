148948c6c241d3c39697d4ead10a15bba11efce3
Pingola <zerogravitystd@gmail.com>
2023-09-12 14:47:15 -0300

    production path

--- lib/apiConfig.ts
+++ lib/apiConfig.ts
@@ -20,4 +20,4 @@ export const API_ENDPOINTS = {
 export const API_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/'
-    : 'productionurl'
+    : 'https://projectg2.vercel.app/'
