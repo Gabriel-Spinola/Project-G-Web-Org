c2cb0bb090b5e07562829ae57527f8d838b9d160
Pingola <zerogravitystd@gmail.com>
2023-09-12 16:35:59 -0300

    a

--- app/api/services/posts/route.ts
+++ app/api/services/posts/route.ts
@@ -5,6 +5,42 @@ import { handleDelete } from './[delete]'
 import { handlePut } from './[put]'
 import { Post } from '@prisma/client'
 
+// REVIEW Read please
+// export const runtime = 'edge';
+
+// const ratelimiter = new Ratelimit({
+//   redis: Redis.fromEnv(),
+//   limiter: Ratelimit.slidingWindow(5, '10 s'),
+//   analytics: true,
+// });
+
+// export async function GET(req: NextRequest) {
+//   // rate limit requests
+//   const ip = req.headers.get('x-forwarded-for');
+//   const { success } = await ratelimiter.limit(ip || 'api');
+//   if (!success) {
+//     return new NextResponse('Rate limit exceeded', { status: 429 });
+//   }
+
+//   const res = await fetch('https://9f5hpdsv6r8j.statuspage.io/api/v2/summary.json', {
+//     next: { revalidate: 60 }, // Revalidate every 60 seconds
+//     // headers: {
+//     //   'Content-Type': 'application/json',
+//     //   'API-Key': process.env.DATA_API_KEY,
+//     // },
+//   });
+
+//   const data = await res.json();
+
+//   return NextResponse.json({ data });
+// }
+
+/**
+ *
+ * @param req Requst
+ * @param id work as authorID for: GET, POST
+ * @returns API Response
+ */
 async function handler(req: Request): Promise<unknown | null> {
   const url = new URL(req.url)
 
