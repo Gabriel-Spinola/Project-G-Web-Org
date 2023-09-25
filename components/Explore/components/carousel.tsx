63a9c76e5309038de33c85848679732b52a843f4
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-09-25 16:44:02 -0300

    explore page features

--- components/Explore/components/carousel.tsx
+++ components/Explore/components/carousel.tsx
@@ -1,31 +1,51 @@
+/**
+ * @author Gabriel Spinola Mendes da Silva | gabrielspinola77@gmail.com
+ * @author Lucas Vinicius Pereira Martis | lucasvinipessoal@gmail.com
+ *
+ * @project Project G
+ * @version main-release
+ * @license i.e. MIT
+ */
+
+'use client'
+
 import { Icon } from '@chakra-ui/react'
 import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons'
+import { MdOutlineKeyboardDoubleArrowDown } from 'react-icons/md'
 
 export default function Carousel() {
   return (
-    <div id="carousel" className="flex gap-4 items-center">
-      <ArrowLeftIcon w={8} h={8} color={'white'} />
-      <div
-        id="item-1"
-        className="w-[295px] h-[261px] bg-darker-white rounded-xl"
-      ></div>
-      <div
-        id="item-1"
-        className="w-[295px] h-[261px] bg-darker-white rounded-xl"
-      ></div>
-      <div
-        id="item-1"
-        className="w-[295px] h-[261px] bg-darker-white rounded-xl"
-      ></div>
-      <div
-        id="item-1"
-        className="w-[295px] h-[261px] bg-darker-white rounded-xl"
-      ></div>
-      <div
-        id="item-1"
-        className="w-[295px] h-[261px] bg-darker-white rounded-xl"
-      ></div>
-      <ArrowRightIcon w={8} h={8} color={'white'} />
+    <div className="flex flex-col gap-16">
+      <section id="carousel" className="flex gap-4 items-center">
+        <ArrowLeftIcon w={8} h={8} color={'white'} />
+        <a
+          href="/"
+          id="item-1"
+          className="w-[295px] h-[261px] bg-darker-white rounded-xl"
+        ></a>
+        <a
+          href="/"
+          id="item-2"
+          className="w-[295px] h-[261px] bg-darker-white rounded-xl"
+        ></a>
+        <a
+          href="/"
+          id="item-3"
+          className="w-[295px] h-[261px] bg-darker-white rounded-xl"
+        ></a>
+        <a
+          href="/"
+          id="item-4"
+          className="w-[295px] h-[261px] bg-darker-white rounded-xl"
+        ></a>
+        <a
+          href="/"
+          id="item-5"
+          className="w-[295px] h-[261px] bg-darker-white rounded-xl"
+        ></a>
+        <ArrowRightIcon w={8} h={8} color={'white'} />
+      </section>
+      <Icon as={MdOutlineKeyboardDoubleArrowDown} />
     </div>
   )
 }
