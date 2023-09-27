471f0228d95530c61848d33c824a1c6ed1a71315
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-09-27 15:17:35 -0300

    more cleanup

--- app/client/profile/actions.ts
+++ app/client/profile/actions.ts
@@ -1,24 +1,8 @@
 import { API_ENDPOINTS, API_URL } from '@/lib/apiConfig'
+import { ESResponse } from '@/lib/common'
 import { User } from '@prisma/client'
 
 export type UserSelectedData = { [key in keyof Partial<User>]: boolean }
-
-/**
- * @author Gabriel Spinola
- *
- * ES stands for error/success response,
- * meaning that: it either returns an error with data being null
- * or the other way around.
- */
-export type ESResponse<DataType> =
-  | {
-      data: DataType
-      error: null
-    }
-  | {
-      data: null
-      error: string
-    }
 
 /**
  *
