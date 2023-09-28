b6a72085d27f3248a0a0c4b6eb56a54136b7845f
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-09-28 14:32:55 -0300

    cleaning up

---
+++ lib/common.d.ts
@@ -0,0 +1,16 @@
+/**
+ * @author Gabriel Spinola
+ *
+ * @summary ES stands for error/success response,
+ * meaning that: it either returns an error with data being null
+ * or the other way around.
+ */
+export type ESResponse<DataType> =
+  | {
+      data: DataType
+      error: null
+    }
+  | {
+      data: null
+      error: string
+    }
