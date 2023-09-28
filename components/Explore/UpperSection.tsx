91f7035271735b50c7ae9329661fd2055732d1b3
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-09-28 15:10:28 -0300

    reponsiviness

--- components/Explore/UpperSection.tsx
+++ components/Explore/UpperSection.tsx
@@ -15,13 +15,13 @@ import Carousel from './components/carousel'
 
 export default function UpperSection() {
   return (
-    <div className="h-[100vh] w-[100%] flex flex-col items-center">
+    <div className="h-[95vh] w-[100%] flex flex-col items-center">
       <section className="absolute z-[1] flex flex-col gap-32 my-24">
         <ExploreSearchBar />
         <Carousel />
       </section>
 
-      <div className="absolute h-[100vh] w-[100%] bg-black/75" />
+      <div className="absolute h-[95vh] w-[100%] bg-black/75" />
       <Image
         className="h-full w-full object-cover object-center"
         src="https://images.unsplash.com/photo-1619615713569-fe374aad7185?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
