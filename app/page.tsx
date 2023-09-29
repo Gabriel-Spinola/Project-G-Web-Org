0d8884b6645305c4f5a6309200191b5cd6008a5b
gseven-gdv <zerogravitystd@gmail.com>
2023-09-29 17:12:08 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-// LINK: https://www.youtube.com/watch?v=ytkG7RT6SvU
-
 import PostSubmitFragment from '@/components/posts/poster/PostSubmitFragment'
 import { fetchPosts } from './feedActions'
 import { ESResponse, FullPost } from '@/lib/common'
 import InfiniteScrollPosts from '@/components/posts/InfiniteScrollPosts'
 
-// LINK - https://www.youtube.com/watch?v=IFYFezylQlI
 export default async function Home() {
   const { data, error }: ESResponse<FullPost[]> = await fetchPosts()
 
