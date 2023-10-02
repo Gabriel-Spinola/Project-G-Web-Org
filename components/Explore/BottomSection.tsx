7e41e37c36851f9c755afeb81b87b4832ad12c5b
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-10-02 15:35:02 -0300

    corrections in responsiviness and grid feature

--- components/Explore/BottomSection.tsx
+++ components/Explore/BottomSection.tsx
@@ -2,7 +2,7 @@ import ExploreHighlight from './components/ExploreHighlights'
 
 export default function BottomSection() {
   return (
-    <section className="bg-darker-white min-w-full flex flex-col items-center">
+    <section className="bg-darker-white flex flex-col items-center">
       <h1 className="m-16 text-xl">VEJA O QUE ESTÁ EM ALTA</h1>
       <ExploreHighlight />
     </section>
