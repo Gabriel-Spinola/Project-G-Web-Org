d1b6f6459f53eb16888a7989adddf508a894e711
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-10-08 15:28:20 -0300

    Revert "functional login page"
    This reverts commit 982517c393d796072e19aaf60b7fac769a2b1919.

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,14 +1,3 @@
-/**
- * @author Gabriel Spinola Mendes da Silva | gabrielspinola77@gmail.com
- * @author Lucas Vinicius Pereira Martis | lucasvinipessoal@gmail.com
- *
- * @project Project G
- * @version main-release
- * @license i.e. MIT
- */
-
-import SigninBox from './components/SigninBox'
-
 export default function Signin() {
-  return <SigninBox />
+  return <h1>Signin</h1>
 }
