43ffc38e55f786b21bf0069bb55d8837c8f6c667
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-10-15 08:45:49 -0300

    post submit changes

--- components/Buttons/SendImageButton.tsx
+++ components/Buttons/SendImageButton.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent } from 'react'
+import { AiOutlineFileImage } from 'react-icons/ai'
 
 type Props = {
   onChange: (event: ChangeEvent<HTMLInputElement>) => void
@@ -6,32 +7,23 @@ type Props = {
 
 export default function SendImageButton({ onChange }: Props) {
   return (
-    <div className="notClose img-btn mr-[280px]">
-      <button className="notClose p-1 relative overflow-hidden flex flex-row mt-8 text-medium-primary select-none text-sm hover:text-darker-white border-solid border-darker-white border-2 rounded bg-darker-white hover:bg-medium-primary hover:border-medium-primary">
-        <i className="notClose importImg">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            className="notClose mr-2"
-          >
-            <path
-              fill="currentColor"
-              d="M5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21H5Zm0-2h14V5H5v14Zm1-2h12l-3.75-5l-3 4L9 13l-3 4Zm-1 2V5v14Z"
-            />
-          </svg>
-        </i>
-        <input
-          type="file"
-          name="images"
-          accept=".png, .jpg, .jpeg, .webp"
-          className="notClose absolute left-0 top-1 scale-150 scale-y-150 opacity-0"
-          onChange={onChange}
-          multiple
-        />
-        Envie uma imagem
-      </button>
+    <div className="img-btn hover:cursor-pointer z-50">
+      <input
+        type="file"
+        name="images"
+        id="images"
+        accept=".png, .jpg, .jpeg, .webp"
+        className="hidden"
+        onChange={onChange}
+        multiple
+      />
+      <label
+        htmlFor="images"
+        className="p-2 flex w-[240px] bg-darker-white text-medium-primary hover:bg-medium-primary hover:text-darker-white cursor-pointer rounded-sm"
+      >
+        <AiOutlineFileImage size={28} />
+        Envie uma Imagem
+      </label>
     </div>
   )
 }
