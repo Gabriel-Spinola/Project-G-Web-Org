bc013d5cd2e1a319f2ed4e7c6e4d2c3d154f6590
gseven-gdv <zerogravitystd@gmail.com>
2023-10-15 13:27:32 -0300

    Update postSchema.ts

--- lib/schemas/postSchema.ts
+++ lib/schemas/postSchema.ts
@@ -1,9 +1,18 @@
 import { z as zod } from 'zod'
+import { ESResponse } from '../types/common'
 
 export type ExpectedData = {
   content: string
-  images: File[] | null
+  images?: File[] | null
 }
+
+export const MAX_FILE_SIZE = 500000
+const ACCEPTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/webp',
+]
 
 export const newPostDataSchema = zod.object({
   content: zod
@@ -12,8 +21,33 @@ export const newPostDataSchema = zod.object({
     .max(4000, {
       message: 'Seu post ultrapassou o limite de caracteres (4000)',
     }),
+  images: zod
+    .any()
+    .optional()
+    .refine(
+      (file: File) => file.size <= MAX_FILE_SIZE,
+      'O tamanho máximo de imagens é de até 5mb',
+    )
+    .refine(
+      (file: File) => ACCEPTED_IMAGE_TYPES.includes(file.type),
+      'Somente images com os formatos: .jpg, .jpeg, .png and .webp são suportadas.',
+    ),
 })
 
-// export function validateForm() {
+/**
+ * @param formData
+ * @returns Either the expected data from the form, or a ZodError object with error info
+ */
+export function validateForm(
+  formData: FormData,
+): ESResponse<ExpectedData, zod.ZodError<ExpectedData>> {
+  const parsedFormData = newPostDataSchema.safeParse(
+    Object.fromEntries(formData.entries()),
+  )
 
-// }
+  if (!parsedFormData.success) {
+    return { data: null, error: parsedFormData.error }
+  }
+
+  return { data: parsedFormData.data, error: null }
+}
