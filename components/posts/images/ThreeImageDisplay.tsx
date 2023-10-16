83fb76d24128d29b4ed81ac308dbdbbd53369491
Gabriel <gabrielspinola77@gmail.com>
2023-10-16 13:28:56 -0300

    -m

--- components/posts/images/ThreeImageDisplay.tsx
+++ components/posts/images/ThreeImageDisplay.tsx
@@ -5,37 +5,50 @@ import Image from 'next/image'
 interface Props {
   imgSrc: string
   secondImgSrc: string
+  thirdImgSrc: string
   width: number
   height: number
+  heightOne: number
 }
 
-export default function TwoImageDisplay({
+export default function ThreeImageDisplay({
   imgSrc,
   secondImgSrc,
+  thirdImgSrc,
   width,
   height,
+  heightOne,
 }: Props) {
   return (
     <section className="w-full h-full rounded-lg overflow-hidden max-h-[480px] bg-medium-gray/75 flex hover:brightness-90 hover:cursor-pointer">
-      <div className="flex justify-center">
+      <Image
+        src={imgSrc}
+        alt={'Imagem Do Post'}
+        width={width}
+        height={heightOne}
+        objectFit="cover"
+        sizes="(max-width: 100%)"
+        style={{ objectFit: 'contain' }}
+        priority
+      />
+      <div className="flex flex-col">
         <Image
-          src={imgSrc}
-          alt={'Imagem Do Post'}
-          width={width}
-          height={height}
-          sizes="(max-width: 384px)"
-          style={{ objectFit: 'contain' }}
-          priority
-        />
-      </div>
-      <div className="flex justify-center">
-        <Image
-          className="rounded-lg"
           src={secondImgSrc}
           alt={'Imagem Do Post'}
           width={width}
           height={height}
-          sizes="(max-width: 384px)"
+          objectFit="cover"
+          sizes="(max-width: 100%)"
+          style={{ objectFit: 'contain' }}
+          priority
+        />
+        <Image
+          src={thirdImgSrc}
+          alt={'Imagem Do Post'}
+          width={width}
+          height={height}
+          objectFit="cover"
+          sizes="(max-width: 100%)"
           style={{ objectFit: 'contain' }}
           priority
         />
