be148b65bc87b97edc274fbe8afc47b518453ab0
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-10-16 13:29:53 -0300

    Create OneImageDisplay.tsx

---
+++ components/posts/images/OneImageDisplay.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import Image from 'next/image'
+
+interface Props {
+  imgSrc: string
+  width: number
+  height: number
+}
+
+export default function OneImageDisplay({ imgSrc, width, height }: Props) {
+  return (
+    <div
+      className={`w-full rounded-lg object-cover bg-medium-gray/75 hover:brightness-90 hover:cursor-pointer hover:`}
+    >
+      <Image
+        className="rounded-lg object-cover"
+        src={imgSrc}
+        alt={'Imagem Do Post'}
+        width={width}
+        height={height}
+        sizes="(max-width: 100%)"
+        priority
+      />
+    </div>
+  )
+}
