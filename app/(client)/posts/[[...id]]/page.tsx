0dcf8b83cbc52889d4885e68bf7b1def82223cf6
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-10-24 15:22:59 -0300

    minor changes

--- app/(client)/posts/[[...id]]/page.tsx
+++ app/(client)/posts/[[...id]]/page.tsx
@@ -1,13 +1,9 @@
-import { fetchPosts } from '@/app/(feed)/_actions'
-import PostHeader from '@/components/posts/PostHeader'
-import { ESResponse } from '@/lib/types/common'
 import React from 'react'
 
 export default async function PostPage() {
   return (
-    <main>
-      <section id="postSection"></section>
-      <section id="commentSection"></section>
+    <main className="flex">
+      <section id="postContent" className=""></section>
     </main>
   )
 }
