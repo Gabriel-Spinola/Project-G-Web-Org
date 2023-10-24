92262ebc3066a70bcc74f9bf61a906a4f6754181
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-10-24 15:51:00 -0300

    Create login.schema.ts

---
+++ lib/schemas/login.schema.ts
@@ -0,0 +1,43 @@
+/**
+ * @author Gabriel Spinola Mendes da Silva
+ *
+ * @project Project G
+ * @license GPL 3.0
+ */
+
+import { z as zod } from 'zod'
+import { ESResponse } from '../types/common'
+
+export type ExpectedData = {
+  email: string
+  password: string
+}
+
+export const dataSchema = zod.object({
+  email: zod
+    .string({ required_error: 'Por favor insira o seu email' })
+    .toLowerCase()
+    .email({ message: 'Insira um email válido' }),
+  password: zod
+    .string({ required_error: 'Por favor insira o seu nome' })
+    .max(64, { message: 'Sua senha deve conter no máximo 50 caracteres' })
+    .min(6, { message: 'Sua senha deve conter no mínimo 6 caracteres' }),
+})
+
+/**
+ * @param formData
+ * @returns Either the expected data from the form, or a ZodError object with error info
+ */
+export function validateForm(
+  formData: FormData,
+): ESResponse<ExpectedData, zod.ZodError<ExpectedData>> {
+  const parsedFormData = dataSchema.safeParse(
+    Object.fromEntries(formData.entries()),
+  )
+
+  if (!parsedFormData.success) {
+    return { data: null, error: parsedFormData.error }
+  }
+
+  return { data: parsedFormData.data, error: null }
+}
