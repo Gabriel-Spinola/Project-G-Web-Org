c7e8903cd2ddfa6acb17d7c97ab2d9df11e3304f
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-10-24 03:04:26 -0300

    sigin style / routing corrections / replace a tags to Link

--- app/(auth)/recover/page.tsx
+++ app/(auth)/recover/page.tsx
@@ -9,14 +9,14 @@
 
 'use client'
 
-import { BsFillArrowLeftCircleFill } from 'react-icons/bs'
-import { BgImage } from '@/components/BgImage'
 import TextBox from '../components/TextBox'
 import { SubmitButton } from '../components/SubmitButton'
 import { verifyCaptcha } from '@/server/serverActions'
 import { useRef, useState } from 'react'
 import ReCAPTCHA from 'react-google-recaptcha'
 import { signIn } from 'next-auth/react'
+import { StaticImage } from '@/components/Image'
+import Link from 'next/link'
 
 export default function RecoverPage() {
   const email = useRef('')
@@ -42,54 +42,62 @@ export default function RecoverPage() {
     })
   }
   return (
-    <main className="min-w-full flex max-w-full h-[calc(100vh-88px)]">
-      <BgImage
-        url={
-          'https://images.unsplash.com/photo-1633354574427-b0dd0697130a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2089&q=80'
-        }
-        alt={'SingInBgImage'}
-      />
-      <section
-        id="loginContainer"
-        className="min-w-full min-h-full flex items-center justify-center"
+    <main className="min-w-full flex max-w-full h-[calc(100vh-88px)] items-center justify-center bg-darker-white">
+      {/* Login Container with Background Image */}
+      <div
+        className={`absolute flex flex-col items-center rounded-xl w-[90vw] h-[90vh]`}
       >
-        <div
-          className={`absolute flex flex-col items-center rounded-xl bg-darker-white/75 border-solid border-2 border-light-white text-darker-white p-16`}
-        >
-          <form className={`flex-col`} onSubmit={handleRecoverySubmission}>
-            <a
-              href="/auth"
-              className="hover:text-medium-secundary absolute -my-10 -mx-10"
+        {/* Background Image for Login Container */}
+        <StaticImage
+          className="w-[90vw] h-[90vh] rounded-xl scale-x-[-1] object-cover"
+          url="https://ebqqbabyixbmiwalviko.supabase.co/storage/v1/object/public/Vampeta-Images-Public/static-images/wolfgang-hasselmann-eSLZXmnw0e8-unsplash.jpg"
+          alt="House Image"
+        />
+        {/* Background Image darker overlay */}
+        <div className="absolute w-full h-full bg-gradient-to-r from-black via-black/60 via-50% to-black/25 rounded-xl">
+          {/* Form Container */}
+          <div className="absolute w-full md:w-[65%] x1:w-[45%] 2x1:w-[35%] float-left h-full rounded-xl text-darker-white">
+            <form
+              className={`flex flex-col justify-evenly w-full h-full gap-4 items-center px-8 md:px-16`}
+              onSubmit={handleRecoverySubmission}
             >
-              <BsFillArrowLeftCircleFill size={32} />
-            </a>
+              <h1 className="md:text-base lg:text-lg x1:text-3xl mb-8 font-bold">
+                {' '}
+                RECUPERAR SENHA{' '}
+              </h1>
 
-            <h1 className="md:text-base lg:text-lg x1:text-3xl mb-8 font-bold">
-              {' '}
-              RECUPERAR SENHA{' '}
-            </h1>
+              <TextBox
+                className="w-full"
+                labelText="E-mail"
+                type={'email'}
+                onChange={(e) => (email.current = e.target.value)}
+              />
 
-            <TextBox
-              className="w-full"
-              labelText="E-mail"
-              type={'email'}
-              onChange={(e) => (email.current = e.target.value)}
-            />
+              <ReCAPTCHA
+                sitekey={process.env.RECAPTCHA_SITE_KEY as string}
+                ref={recaptchaRef}
+                onChange={handleCaptchaSubmission}
+                className="my-4"
+              />
 
-            <ReCAPTCHA
-              sitekey={process.env.RECAPTCHA_SITE_KEY as string}
-              ref={recaptchaRef}
-              onChange={handleCaptchaSubmission}
-              className="my-4"
-            />
-
-            <SubmitButton
-              isVerified={isVerified}
-              buttonText={'ENVIAR E-MAIL DE RECUPERAÇÃO'}
-            />
-          </form>
+              <SubmitButton
+                isVerified={isVerified}
+                buttonText={'ENVIAR E-MAIL DE RECUPERAÇÃO'}
+              />
+              <p className="text-center">
+                Já possui Conta?{' '}
+                <Link
+                  href="/login"
+                  id="formButton"
+                  className="text-light-primary underline hover:text-darker-primary font-bold"
+                >
+                  Voltar
+                </Link>{' '}
+              </p>
+            </form>
+          </div>
         </div>
-      </section>
+      </div>
     </main>
   )
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -9,7 +9,7 @@
 
 'use client'
 
-import { registerNewUser } from '@/app/auth/_actions'
+import { registerNewUser } from '../_actions'
 import { ESResponse } from '@/lib/types/common'
 import { verifyCaptcha } from '@/server/serverActions'
 import { signIn } from 'next-auth/react'
@@ -19,7 +19,8 @@ import { validateRegisterForm } from '@/lib/schemas/userRegisteringSchema'
 import { SubmitButton } from '../components/SubmitButton'
 import TextBox from '../components/TextBox'
 import { FcGoogle } from 'react-icons/fc'
-import { BgImage } from '@/components/BgImage'
+import { StaticImage } from '@/components/Image'
+import Link from 'next/link'
 
 export default function RegisterPage() {
   const [isVerified, setIsVerified] = useState<boolean>(false)
@@ -75,86 +76,91 @@ export default function RegisterPage() {
   }
 
   return (
-    <main className="min-w-full flex max-w-full h-[calc(100vh-88px)]">
-      <BgImage
-        url={
-          'https://images.unsplash.com/photo-1633354574427-b0dd0697130a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2089&q=80'
-        }
-        alt={'SingInBgImage'}
-      />
-      <section
-        id="loginContainer"
-        className="min-w-full min-h-full flex items-center justify-center"
+    // First Wrapper Component
+    <main className="min-w-full flex max-w-full h-[calc(100vh-88px)] items-center justify-center bg-darker-white">
+      {/* Login Container with Background Image */}
+      <div
+        className={`absolute flex flex-col items-center rounded-xl w-[90vw] h-[90vh]`}
       >
-        <div
-          className={`absolute flex flex-col items-center rounded-xl bg-darker-white/75 border-solid border-2 border-light-white text-darker-white p-16`}
-        >
-          <form
-            ref={formRef}
-            action={handleFormSubmission}
-            className={`flex-col w-full gap-4 items-center}`}
-          >
-            <h1 className="text-center text-2xl mb-8">Crie sua conta</h1>
-            <TextBox
-              className="w-full"
-              labelText="Nome"
-              htmlForId="name"
-              type={'text'}
-            />
-
-            <TextBox
-              className="w-full"
-              labelText="E-mail"
-              htmlForId="email"
-              type={'email'}
-            />
-
-            <TextBox
-              className="w-full"
-              labelText="Senha"
-              htmlForId="password"
-              type={'password'}
-            />
-
-            <TextBox
-              className="w-full"
-              labelText="Confime a senha"
-              htmlForId="confirmPassword"
-              type={'password'}
-            />
-
-            <ReCAPTCHA
-              sitekey={process.env.RECAPTCHA_SITE_KEY as string}
-              ref={recaptchaRef}
-              onChange={handleCaptchaSubmission}
-              className="my-5"
-            />
-
-            <button
-              onClick={() => signIn('google')}
-              className="w-full flex justify-around items-center text-xl bg-pure-white rounded-lg p-4 mb-4 text-light-gray"
+        {/* Background Image for Login Container */}
+        <StaticImage
+          className="w-[90vw] h-[90vh] rounded-xl scale-x-[-1] object-cover"
+          url="https://ebqqbabyixbmiwalviko.supabase.co/storage/v1/object/public/Vampeta-Images-Public/static-images/wolfgang-hasselmann-eSLZXmnw0e8-unsplash.jpg"
+          alt="House Image"
+        />
+        {/* Background Image darker overlay */}
+        <div className="absolute w-full h-full bg-gradient-to-r from-black via-black/60 via-50% to-black/25 rounded-xl">
+          {/* Form Container */}
+          <div className="absolute w-full md:w-[65%] x1:w-[45%] 2x1:w-[35%] float-left h-full rounded-xl text-darker-white">
+            <form
+              ref={formRef}
+              action={handleFormSubmission}
+              className={`flex flex-col justify-evenly w-full h-full gap-4 items-center px-8 md:px-16`}
             >
-              <FcGoogle size={36} />
-              <p>
-                Entrar com <span className="font-semibold">GOOGLE</span>
+              <h1 className="text-xl md:text-xl lg:text-2xl font-bold text-center">
+                Crie sua conta
+              </h1>
+              <TextBox
+                className="w-full"
+                labelText="Nome"
+                htmlForId="name"
+                type={'text'}
+              />
+
+              <TextBox
+                className="w-full"
+                labelText="E-mail"
+                htmlForId="email"
+                type={'email'}
+              />
+
+              <TextBox
+                className="w-full"
+                labelText="Senha"
+                htmlForId="password"
+                type={'password'}
+              />
+
+              <TextBox
+                className="w-full"
+                labelText="Confime a senha"
+                htmlForId="confirmPassword"
+                type={'password'}
+              />
+
+              <ReCAPTCHA
+                sitekey={process.env.RECAPTCHA_SITE_KEY as string}
+                ref={recaptchaRef}
+                onChange={handleCaptchaSubmission}
+              />
+
+              <div id="submitRegiter" className="flex w-full gap-4">
+                <SubmitButton
+                  isVerified={isVerified}
+                  buttonText={'REGISTRAR'}
+                />
+                <button
+                  onClick={() => signIn('google')}
+                  className="flex justify-around items-center text-xl bg-pure-white rounded-lg p-2 hover:scale-[101%]"
+                >
+                  <FcGoogle size={36} />
+                </button>
+              </div>
+
+              <p className="text-center">
+                Já possui Conta?{' '}
+                <Link
+                  href="/login"
+                  id="formButton"
+                  className="text-light-primary underline hover:text-darker-primary font-bold"
+                >
+                  Logue Aqui
+                </Link>{' '}
               </p>
-            </button>
-
-            <SubmitButton isVerified={isVerified} buttonText={'REGISTRAR'} />
-
-            <p className="text-center">
-              Já possui Conta?{' '}
-              <a
-                href="/auth"
-                id="formButton"
-                className="text-light-secundary underline hover:text-darker-secundary font-bold"
-              >
-                Logue Aqui
-              </a>{' '}
-            </p>
-          </form>
+            </form>
+          </div>
         </div>
-      </section>
+      </div>
     </main>
   )
 }
