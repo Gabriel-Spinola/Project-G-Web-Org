94c233116104643e8c766764c14c9d4cc5925832
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-10-25 16:55:19 -0300

    minor changes

--- app/api/services/posts/_post.ts
+++ app/api/services/posts/_post.ts
@@ -1,36 +1,10 @@
 import { prisma } from '@/lib/database/prisma'
-import { FileBody, StorageResponse } from '@/lib/storage/storage'
-import { SUPABASE_PUBLIC_BUCKET_NAME, supabase } from '@/lib/storage/supabase'
+import { storeImage } from '@/lib/storage/actions'
 import { Post } from '@prisma/client'
 import { revalidateTag } from 'next/cache'
 import { NextResponse } from 'next/server'
 
 // export const config = { runtime: 'experimental-edge' }
-
-async function storeImage(
-  authorId: string,
-  fileName: string,
-  images: FileBody,
-): Promise<StorageResponse> {
-  try {
-    // FIXME - failing at certain types of images
-    const { data, error } = await supabase.storage
-      .from(SUPABASE_PUBLIC_BUCKET_NAME)
-      .upload(`posts/${authorId}/${fileName}`, images, {
-        cacheControl: '3600',
-        upsert: true,
-      })
-
-    if (error) {
-      throw error
-    }
-
-    return data
-  } catch (error: unknown) {
-    console.error('failed at image storage ' + error)
-    throw error
-  }
-}
 
 async function createPost(
   newPost: Partial<Post>,
---
+++ app/api/services/users/image/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import handlePut from './_put'
+
+async function handler(req: Request): Promise<NextResponse> {
+  const url = new URL(req.url)
+  const id: string | null = url.searchParams.get('id')
+
+  if (!id) {
+    return NextResponse.json(
+      {
+        data: `FAILED:SERVICES/${req.method}-User::failed: id Can't be null`,
+      },
+      { status: 400 },
+    )
+  }
+
+  if (req.method === 'PUT') {
+    return handlePut(req)
+  }
+
+  return NextResponse.json(
+    { data: { message: 'Invalid method' } },
+    { status: 401 },
+  )
+}
+
+export { handler as GET, handler as PUT }
