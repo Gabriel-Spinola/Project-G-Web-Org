e686ddb46f7d52ecb11b2d0838e6ce4bc17377b9
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-10-27 18:30:16 -0300

    lord help me

--- app/api/services/projects/[id]/route.ts
+++ app/api/services/projects/[id]/route.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from 'next/server'
 import { handlePost } from '../../posts/_post'
 
 export function handler(req: Request, { params }: { params: { id: string } }) {
@@ -6,6 +7,11 @@ export function handler(req: Request, { params }: { params: { id: string } }) {
   switch (req.method) {
     case 'POST':
       return handlePost(id, req)
+    default:
+      return NextResponse.json(
+        { data: { message: 'Invalid method' } },
+        { status: 401 },
+      )
   }
 }
 
