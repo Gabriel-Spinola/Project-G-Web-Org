8f331492e6fbfb52ab109e0f81819841ac11accf
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-10-27 12:21:39 -0300

    testing production routes 2

--- app/(client)/profile/_actions.ts
+++ app/(client)/profile/_actions.ts
@@ -1,9 +1,46 @@
 import { API_ENDPOINTS, API_URL } from '@/lib/apiConfig'
-import { ESResponse } from '@/lib/types/common'
+import { ESResponse, UserData } from '@/lib/types/common'
 import { User } from '@prisma/client'
 import { isFollowing } from './_server-actions'
 
 export type UserSelectedData = { [key in keyof Partial<User>]: boolean }
+
+/**
+ * @author Gabriel Spinola
+ *
+ * @param id Id of the user as a string.
+ * @param requestData the specific data from the user you want to request.
+ * @returns the requested data from the user or, if failed, null.
+ */
+export async function getUserData(
+  id: string,
+  requestData: UserSelectedData,
+): Promise<UserData | null> {
+  try {
+    const response = await fetch(
+      `${API_URL}${API_ENDPOINTS.services.users}?id=${id}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-API-Key': process.env.API_SECRET as string,
+        },
+        body: JSON.stringify(requestData),
+        next: { tags: ['user-data'] },
+      },
+    )
+
+    if (!response.ok) throw new Error('Response not ok')
+
+    const { data }: { data: UserData } = await response.json()
+
+    return data
+  } catch (error: unknown) {
+    console.error(error, 'Failed to fetch users')
+
+    return null
+  }
+}
 
 /**
  *
