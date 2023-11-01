e007f5b961f44fe56ccb66cdeb4f4c79f30775e7
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-11-01 14:35:45 -0300

    post styling

--- components/Buttons/CreateCommentButton.tsx
+++ components/Buttons/CreateCommentButton.tsx
@@ -5,5 +5,9 @@ import React from 'react'
 export default function CreateCommentButton() {
   // const { pending } = useFormStatus()
 
-  return <button type="submit">{'add'}</button>
+  return (
+    <button type="submit" className="">
+      {'Comentar'}
+    </button>
+  )
 }
