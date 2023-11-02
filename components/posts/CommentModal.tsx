0c237c11c086002d696f82c1e6baf9f2d5d14584
Gabriel-Spinola <gabrielspinola77@gmail.com>
2023-11-02 12:19:12 -0300

    a

---
+++ components/posts/CommentModal.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { BiComment } from 'react-icons/bi'
+import { FullPost } from '@/lib/types/common'
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react'
+import PostCommentsSection from './PostCommentSection'
+
+interface Props {
+  commentNumber: number
+  post: FullPost
+  currentUserId?: string
+}
+
+export default function CommentModal({
+  commentNumber,
+  post,
+  currentUserId,
+}: Props) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <button
+      className="flex flex-col justify-center items-center hover:text-medium-primary"
+      onClick={onOpen}
+    >
+      <BiComment size={24} />
+      <span>{commentNumber}</span>
+      <Modal isOpen={isOpen} onClose={onClose} size={'2xl'}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Comentários</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <PostCommentsSection post={post} currentUserId={currentUserId} />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </button>
+  )
+}
