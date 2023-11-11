4234358445ce031a83cb428c3d1dbc902687923f
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-11-11 12:57:50 -0300

    minor changes

--- components/comments/NewCommentDialog.tsx
+++ components/comments/NewCommentDialog.tsx
@@ -27,7 +27,7 @@ export default function NewCommentDialog({
   const router = useRouter()
   const pathName = usePathname()
 
-  async function handleFormSubimission(formData: FormData) {
+  async function handleFormSubmission(formData: FormData) {
     if (!currentUserId) {
       signIn()
 
@@ -70,6 +70,7 @@ export default function NewCommentDialog({
   function inputReplace() {
     const formInput = document.getElementById('contentk') as HTMLInputElement
     const editableDiv = document.getElementById('editablediv') as HTMLDivElement
+
     formInput.value = editableDiv.innerHTML
   }
 
@@ -77,12 +78,13 @@ export default function NewCommentDialog({
     const placeHolder = document.getElementById(
       'commentPlaceHolder',
     ) as HTMLSpanElement
+
     placeHolder.innerHTML = ''
   }
 
   return (
     <form
-      action={handleFormSubimission}
+      action={handleFormSubmission}
       className="flex flex-col justify-end items-end gap-4"
     >
       <input type="hidden" id="contentk" name="content" value="" />
@@ -90,15 +92,9 @@ export default function NewCommentDialog({
         className="bg-darker-white w-full p-2 rounded-md outline-black/25"
         contentEditable
         id="editablediv"
-        onInput={() => {
-          inputReplace()
-        }}
+        onInput={inputReplace}
         onClick={removePlaceHolder}
-      >
-        <span className="opacity-75" id="commentPlaceHolder">
-          Faça um comentário
-        </span>
-      </div>
+      ></div>
 
       <CreateCommentButton />
     </form>
