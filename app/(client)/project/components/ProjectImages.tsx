8025670d5901526df241d6811a92ab27fdb42e1a
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-11-28 14:48:42 -0300

    Update ProjectImages.tsx

--- app/(client)/project/components/ProjectImages.tsx
+++ app/(client)/project/components/ProjectImages.tsx
@@ -29,75 +29,77 @@ export default function ProjectImagesCarousel({
   }
 
   return (
-    <aside className="relative flex flex-col w-full justify-end">
-      {/* NOTE - Image and controllers Wrapper */}
-      <section className="relative h-full w-full">
-        <div className="absolute flex w-full h-full items-center">
-          {currentImageIndex + 1 > 1 && (
-            <button
-              className="absolute flex items-center justify-center left-0 h-full w-16 p-2 text-darker-primary hover:bg-medium-gray/25"
-              onClick={handleImageChangeSubtract}
-            >
-              <div className="shadow-lg">
-                <FaArrowCircleLeft size={32} />
-              </div>
-            </button>
-          )}
-          {/* NOTE - Current Image */}
-          {currentImageIndex + 1 < imagesSrc.length && (
-            <button
-              className="absolute flex items-center justify-center right-0 h-full w-16 p-2 text-darker-primary hover:bg-medium-gray/25"
-              onClick={handleImageChangeAdd}
-            >
-              <FaArrowAltCircleRight size={32} />
-            </button>
-          )}
-        </div>
-        {/* NOTE - Image Wrapper */}
-        <div className="overflow-hidden flex items-center justify-center bg-darker-gray rounded-l-lg w-full h-full">
-          {currentImage && (
-            <Image
-              alt=""
-              src={getImageAbsoluteURLFromPubBucket(
-                `projects/${projectOwner}/images/${currentImage}`,
-              )}
-              className="overflow-hidden object-contain w-full max-h-full"
-            />
-          )}
-        </div>
-      </section>
-      {/* NOTE - Image controllers dots */}
-      {imagesSrc.length > 1 ? (
-        <section
-          id="controllers"
-          className="absolute flex w-full items-center justify-center max-h-full max-w-full p-2"
-        >
-          <div id="selectors" className="flex flex-row gap-8">
-            {imagesSrc.map((_, index) => (
-              <>
-                <button
-                  key={index}
-                  onClick={() => {
-                    setCurrentImageIndex(index)
-                  }}
-                >
-                  <div
-                    className={`rounded-full ${
-                      currentImageIndex === index
-                        ? ' bg-medium-primary'
-                        : 'bg-medium-gray'
-                    } p-2 h-2`}
-                  >
-                    {' '}
-                  </div>
-                </button>
-              </>
-            ))}
+    <>
+      <aside className="relative flex flex-col w-full justify-end">
+        {/* NOTE - Image and controllers Wrapper */}
+        <section className="relative h-full w-full">
+          <div className="absolute flex w-full h-full items-center">
+            {currentImageIndex + 1 > 1 && (
+              <button
+                className="absolute flex items-center justify-center left-0 h-full w-16 p-2 text-darker-primary hover:bg-medium-gray/25"
+                onClick={handleImageChangeSubtract}
+              >
+                <div className="shadow-lg">
+                  <FaArrowCircleLeft size={32} />
+                </div>
+              </button>
+            )}
+            {/* NOTE - Current Image */}
+            {currentImageIndex + 1 < imagesSrc.length && (
+              <button
+                className="absolute flex items-center justify-center right-0 h-full w-16 p-2 text-darker-primary hover:bg-medium-gray/25"
+                onClick={handleImageChangeAdd}
+              >
+                <FaArrowAltCircleRight size={32} />
+              </button>
+            )}
+          </div>
+          {/* NOTE - Image Wrapper */}
+          <div className="overflow-hidden flex items-center justify-center bg-darker-gray rounded-l-lg w-full h-full">
+            {currentImage && (
+              <Image
+                alt=""
+                src={getImageAbsoluteURLFromPubBucket(
+                  `projects/${projectOwner}/images/${currentImage}`,
+                )}
+                className="overflow-hidden object-contain w-full max-h-full"
+              />
+            )}
           </div>
         </section>
-      ) : (
-        ''
-      )}
-    </aside>
+        {/* NOTE - Image controllers dots */}
+        {imagesSrc.length > 1 ? (
+          <section
+            id="controllers"
+            className="absolute flex w-full items-center justify-center max-h-full max-w-full p-2"
+          >
+            <div id="selectors" className="flex flex-row gap-8">
+              {imagesSrc.map((_, index) => (
+                <>
+                  <button
+                    key={index}
+                    onClick={() => {
+                      setCurrentImageIndex(index)
+                    }}
+                  >
+                    <div
+                      className={`rounded-full ${
+                        currentImageIndex === index
+                          ? ' bg-medium-primary'
+                          : 'bg-medium-gray'
+                      } p-2 h-2`}
+                    >
+                      {' '}
+                    </div>
+                  </button>
+                </>
+              ))}
+            </div>
+          </section>
+        ) : (
+          ''
+        )}
+      </aside>
+    </>
   )
 }
