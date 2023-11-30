c5f25a85992b6b50c956dd3889d9041916ceecb5
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-11-30 14:16:00 -0300

    Update BgImage.tsx

--- components/BgImage.tsx
+++ components/BgImage.tsx
@@ -7,15 +7,15 @@ interface Params {
   alt: string
 }
 
-export const BgImage = ({ url, alt }: Params) => {
+export const BgImage = (params: Params) => {
   return (
     <section>
       <Image
         className="absolute w-full h-[calc(100vh-88px)] object-cover"
-        src={url}
-        alt={alt}
+        src={params.url}
+        alt={params.alt}
       />
-      <div className="absolute w-full h-[calc(100vh-88px)] bg-[black]/75" />
+      <div className="absolute w-full h-[calc(100vh-88px)] bg-black/75" />
     </section>
   )
 }
