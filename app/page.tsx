4d11128a64b5029896c119dc1df9f1043c22233d
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-12-06 14:12:52 -0300

    side bar

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,7 @@ import { Suspense } from 'react'
 import NewPostModal from '@/components/posts/postSubmit/NewPostModal'
 import { getServerSession } from 'next-auth'
 import { AuthOptions } from '@/lib/auth'
-import Link from 'next/link'
-import { MdPushPin } from 'react-icons/md'
-import { Avatar } from '@chakra-ui/avatar'
-import { getProfilePicURL } from '@/lib/uiHelpers/profilePicActions'
-import { User } from '@prisma/client'
+import FeedSideBar from '@/components/feed/FeedSideBar'
 
 export default async function Home() {
   const posts = await fetchPosts()
@@ -28,22 +24,7 @@ export default async function Home() {
 
   return (
     <main className="relative flex min-h-[calc(100vh-88px)] justify-around flex-row bg-darker-white mt-[88px]">
-      <section className="hidden x1:block x1:fixed x1:left-0]">
-        <article className="flex flex-col">
-          <div>
-            <Avatar
-              size={'2xl'}
-              src={getProfilePicURL(session?.user as User)}
-            />
-          </div>
-          <Link
-            className="w-full flex px-8 py-4 bg-darker-white border-b-2 border-medium-primary hover:brightness-75"
-            href={`/profile/${session?.user.id}/pins/`}
-          >
-            Veja seus posts salvos <MdPushPin size={16} />
-          </Link>
-        </article>
-      </section>
+      {session && <FeedSideBar />}
 
       <section className="flex gap-8 mt-8 flex-col items-center min-w-full sm:min-w-[480px] md:min-w-[680px] md:max-w-[680px]">
         <div className="w-full">
---
+++ components/feed/FeedSideBar.tsx
@@ -0,0 +1,45 @@
+import { AuthOptions } from '@/lib/auth'
+import { getProfilePicURL } from '@/lib/uiHelpers/profilePicActions'
+import { Avatar } from '@chakra-ui/avatar'
+import { User } from '@prisma/client'
+import { getServerSession } from 'next-auth'
+import Link from 'next/link'
+import React from 'react'
+import { BsFillGearFill } from 'react-icons/bs'
+import { MdPushPin } from 'react-icons/md'
+
+export default async function FeedSideBar() {
+  const session = await getServerSession(AuthOptions)
+  if (session)
+    return (
+      <section className="hidden x1:flex x1:flex-col gap-4 x1:fixed left-0 py-8 px-4">
+        <article className="flex gap-4">
+          <Avatar size={'lg'} src={getProfilePicURL(session.user as User)} />
+          <div className="flex flex-col justify-evenly">
+            <h1 className="text-xl">{session.user.name}</h1>
+            <Link
+              href={`/profile/${session.user.id}`}
+              className="hover:underline"
+            >
+              Ir para Perfil
+            </Link>
+          </div>
+        </article>
+        <article className="flex flex-col w-full gap-4">
+          <Link
+            className="w-full flex gap-4 justify-between items-center py-4 bg-darker-white border-b-2 border-medium-primary hover:brightness-75"
+            href={`/profile/${session.user.id}/pins/`}
+          >
+            Veja seus posts salvos <MdPushPin size={16} />
+          </Link>
+
+          <Link
+            className="w-full flex gap-4 justify-between items-center py-4 bg-darker-white border-b-2 border-medium-primary hover:brightness-75"
+            href={`/profile/${session.user.id}/user-settings/exhibition`}
+          >
+            Configurações <BsFillGearFill size={16} />
+          </Link>
+        </article>
+      </section>
+    )
+}
