b5d92f08d5d9e9c26524a4c3d23bb7c8dc0632be
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-12-08 15:14:50 -0300

    Update route.ts

--- app/api/key/route.ts
+++ app/api/key/route.ts
@@ -6,11 +6,6 @@ export async function POST(req: Request) {
     const message = process.env.NEXTAUTH_SECRET as string
     const secretKey = process.env.DEC_KEY as string
 
-    const { key: givenKey }: { key: string } = await req.json()
-    if (givenKey !== secretKey) {
-      return new Response('Invalid Key', { status: 401 })
-    }
-
     const jwtToken = sign({ data: message }, secretKey, { expiresIn: '30d' })
 
     return new Response(jwtToken, { status: 200 })
