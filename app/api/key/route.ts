5e4520837a9dfc504b0c4b2c33131c9cbdc1332e
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-12-08 15:00:09 -0300

    Update route.ts

--- app/api/key/route.ts
+++ app/api/key/route.ts
@@ -1,15 +1,17 @@
-import { Rabbit } from 'crypto-js'
+import { sign } from 'jsonwebtoken'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function POST(_req: Request) {
+export async function POST(req: Request) {
   try {
     const message = process.env.NEXTAUTH_SECRET as string
-    const key = process.env.DEC_KEY as string
+    const secretKey = process.env.JWT_SECRET as string
 
-    const hash = Rabbit.encrypt(message, key).toString()
+    const jwtToken = sign({ data: message }, secretKey, { expiresIn: '30d' })
 
-    return new Response(hash, { status: 200 })
+    return new Response(jwtToken, { status: 200 })
   } catch (error: unknown) {
+    console.error(error)
+
     return new Response(null, { status: 500 })
   }
 }
