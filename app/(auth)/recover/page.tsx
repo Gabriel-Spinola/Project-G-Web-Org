2a13501110cf9f91f759ccdc9d025d36599a1cef
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-12-07 21:31:36 -0300

    Next image properties fix

--- app/(auth)/recover/page.tsx
+++ app/(auth)/recover/page.tsx
@@ -18,6 +18,7 @@ export default async function LoginPage() {
         className="absolute w-full h-full object-cover"
         url="/assets/explore/photo-1591874204276-1ebd20fb8db6.webp"
         alt="imagem"
+        fill={true}
       />
 
       <section className="w-full md:w-auto lg:w-[30vw] h-full md:h-auto md:max-h-[80vh] rounded-lg drop-shadow-[0_35px_35px_rgba(0,0,0,0.35)] bg-black/10 backdrop-blur-md p-8">
--- components/Image.tsx
+++ components/Image.tsx
@@ -4,6 +4,7 @@ interface Params {
   url: string
   alt: string
   className?: string
+  fill?: boolean
 }
 
 export default function StaticImage(params: Params) {
@@ -15,6 +16,11 @@ export default function StaticImage(params: Params) {
       loading="lazy"
       blurDataURL={params.url}
       placeholder="blur"
+      width={!params.fill ? 100 : undefined}
+      height={!params.fill ? 100 : undefined}
+      fill={params.fill}
+      quality={100}
+      style={{ objectFit: 'fill' }} // The point is right there!
     />
   )
 }
