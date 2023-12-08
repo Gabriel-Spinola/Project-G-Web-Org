b9f2e92e26e2fe19acc4070a886d74b376bb2948
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-12-07 23:44:36 -0300

    logout form correction

--- app/(auth)/components/buttons/LogoutButton.tsx
+++ app/(auth)/components/buttons/LogoutButton.tsx
@@ -7,19 +7,19 @@ import { RiLogoutBoxLine } from 'react-icons/ri'
 
 export default function LogoutButton() {
   return (
-    <div className="flex flex-col w-full h-full gap-8 justify-center p-32">
+    <div className="flex flex-col w-full h-full gap-8 justify-center p-8">
       <h1 className="w-full text-center text-2xl text-pure-white">
         Parece que você já está logado!
       </h1>
       <button
-        className="w-full h-12 rounded-lg flex items-center justify-between pl-5 pr-20 bg-pure-white text-darker-primary font-semibold hover:bg-medium-white"
+        className="w-full h-12 rounded-lg flex items-center justify-evenly bg-pure-white text-darker-primary font-semibold hover:bg-medium-white"
         onClick={() => signOut()}
       >
         <RiLogoutBoxLine size={24} />
         Sair da conta
       </button>
       <Link href="/">
-        <button className="w-full h-12 rounded-lg flex items-center justify-between pl-5 pr-20 bg-pure-white text-darker-primary font-semibold hover:bg-medium-white">
+        <button className="w-full h-12 rounded-lg flex items-center justify-evenly bg-pure-white text-darker-primary font-semibold hover:bg-medium-white">
           <AiFillHome size={24} />
           Página inicial
         </button>
