5bdd5abdfda99742244fabf5466a5760640837b4
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-12-09 09:27:38 -0300

    explore page rework

---
+++ app/(client)/explore/components/ExploreGrid.tsx
@@ -0,0 +1,190 @@
+import Image from 'next/image'
+import './exploreGrid.css'
+
+export default function ExploreGrid() {
+  return (
+    <section className="bg-darker-white flex flex-col items-center w-full">
+      <h1 className="m-16 text-xl">VEJA O QUE ESTÁ EM ALTA</h1>
+      <article className={`gridcontainer`}>
+        <div>
+          <Image
+            src={'/assets/explore/hin-bong-yeung-jF946mh5QrA-unsplash.jpg'}
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1682685796766-0fddd3e480de?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1701822770347-26898fe298e3?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1701195617130-a16e1b794ccc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1701690663879-48ab4a313451?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1683009427470-a36fee396389?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1701719708319-686b2d5f686a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1701894492549-a9a2857d4981?q=80&w=1882&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1690647094432-6aff2e4ca8c1?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1689971038052-b4f630af2412?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1678875203431-be622691df95?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1698586165676-480dc54ba1e3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1610297588220-d2bc81e66398?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1692698921100-e31dc7453d4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+        <div>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1562823319-e4b20dd39568?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
+            alt="Imagem"
+            style={{ objectFit: 'contain' }}
+            width={1000}
+            height={1000}
+            quality={75}
+          />
+        </div>
+      </article>
+    </section>
+  )
+}
--- next.config.mjs
+++ next.config.mjs
@@ -27,6 +27,12 @@ const nextConfig = {
         port: '',
         pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**',
+      },
     ],
   },
   async headers() {
